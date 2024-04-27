9282537230415db741eec141ba0a6221d3d8a4a7
sojun15 <sojun2115@cseku.ac.bd>
2024-04-26 23:05:00 -0700

    At first make main class

---
+++ game.java
@@ -0,0 +1,7 @@
+package com.mycompany.practice;
+public class game {
+    public static void main(String[] args) {
+        int hight = 600, width = 600;
+        Feature ft = new Feature(width,hight);
+    }
+}
